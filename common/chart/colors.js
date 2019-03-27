5ea8a9bfaa7cc51673c12fe73a4ae3c1b7db4d2d
Gabe Abrams <gabeabrams@gmail.com>
2019-03-27 14:06:46 -0700

    Removing everything

---
+++ common/chart/colors.js
@@ -0,0 +1,12 @@
+/* Default colors for use in charts, etc */
+export default {
+  // A pretty neutral blue to use as a default color
+  defaultBlue: '#419bf7',
+
+  // A simple assortment of 14 colors
+  assortment: [
+  // First 10:
+  '#ec4f6a', '#f19542', '#f6c353', '#61cbcc', '#419bf7', '#005392', '#7d5297', '#d261ab', '#7edfcf', '#5eac88',
+  // Last 4:
+  '#f4ce63', '#929000', '#5e5e5e', '#008e00']
+};
