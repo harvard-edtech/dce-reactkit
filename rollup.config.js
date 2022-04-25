54895ed0ac99b5fcd1b89e89a67324b14b79f5b0
Gabe Abrams <gabeabrams@gmail.com>
2022-04-25 16:43:11 -0400

    Added sourcemap plugin

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
 
 const packageJson = require("./package.json");
 
@@ -24,6 +26,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      sourcemaps(),
     ],
     external: [
       ...Object.keys(packageJson.dependencies || {}),
