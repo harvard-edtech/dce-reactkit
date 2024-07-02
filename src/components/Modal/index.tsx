598e23c4880f0f944121586abf179ba233346776
Yuen Ler Chow <lerchow@gmail.com>
2024-07-01 23:36:38 -0700

    small changes

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -14,8 +14,10 @@ import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 
 // Import other components
-import waitMs from '../../helpers/waitMs';
 import LoadingSpinner from '../LoadingSpinner';
+
+// Import helpers
+import waitMs from '../../helpers/waitMs';
 
 // Import types
 import Variant from '../../types/Variant';
@@ -409,6 +411,10 @@ const Modal: React.FC<ModalProps> = (props) => {
     animationClass = 'Modal-animating-pop';
   }
 
+  // default to show close button when not loading and not show close button when loading
+  // unless downShowXButton or isLoadingCancelable
+  const showCloseButton = onClose && !dontShowXButton && !(isLoading && !isLoadingCancelable);
+
   // Render the modal
   const contentToRender = (
     <div
@@ -509,7 +515,7 @@ const Modal: React.FC<ModalProps> = (props) => {
                 {title}
               </h5>
 
-              {(onClose && !dontShowXButton && !(isLoading && !isLoadingCancelable)) && (
+              {showCloseButton && (
                 <button
                   type="button"
                   className="Modal-x-button btn-close"
@@ -545,15 +551,13 @@ const Modal: React.FC<ModalProps> = (props) => {
               ),
             }}
           >
-            {isLoading ? (
+            {isLoading && !children ? (
               <>
                 <LoadingSpinner />
                 <span className="sr-only">Content loading</span>
               </>
             ) : (
-              children && (
-                children
-              )
+              children
             )}
           </div>
 
