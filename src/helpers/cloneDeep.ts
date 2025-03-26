201d9714b54395f3ecb70283633fe3ebbad81351
Yuen Ler Chow <lerchow@gmail.com>
2025-03-26 15:08:08 -0400

    jsdoc

--- src/helpers/cloneDeep.ts
+++ src/helpers/cloneDeep.ts
@@ -1,5 +1,11 @@
 import clone from 'nanoclone';
 
+/**
+ * Deeply clones an object
+ * @author Yuen Ler Chow
+ * @param obj the object to clone
+ * @returns a deep clone of the object
+ */
 const cloneDeep: <T>(obj: T) => T = clone;
 
 export default cloneDeep;
