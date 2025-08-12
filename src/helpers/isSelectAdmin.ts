36839282a28bd7d8d5ec1ce93c5f47ad87cfa575
Gabe Abrams <gabeabrams@gmail.com>
2025-08-12 11:23:51 -0400

    Documentation/naming of variables

--- src/helpers/isSelectAdmin.ts
+++ src/helpers/isSelectAdmin.ts
@@ -1,12 +1,15 @@
+// Import shared constants
 import SELECT_ADMIN_CHECK_ROUTE from '../constants/SELECT_ADMIN_CHECK_ROUTE';
+
+// Import shared helpers
 import visitServerEndpoint from './visitServerEndpoint';
 
 /*------------------------------------------------------------------------*/
 /* ------------------------------- Caching ------------------------------ */
 /*------------------------------------------------------------------------*/
 
-// True if user is a select admin
-let cachedResult: boolean | undefined;
+// If true, user is a select admin
+let cachedUserIsSelectAdmin: boolean | undefined;
 
 /*------------------------------------------------------------------------*/
 /* -------------------------------- Main -------------------------------- */
@@ -18,20 +21,26 @@ let cachedResult: boolean | undefined;
  * @returns true if the user is a select admin, false otherwise
  */
 const isSelectAdmin = async (): Promise<boolean> => {
-  if (cachedResult !== undefined) {
-    return cachedResult;
+  // Use cached version if we have one
+  if (cachedUserIsSelectAdmin !== undefined) {
+    return cachedUserIsSelectAdmin;
   }
 
+  // Check if the user is a select admin
   try {
-    let check = false;
-    check = await visitServerEndpoint({
+    const userIsSelectAdmin: boolean = await visitServerEndpoint({
       path: SELECT_ADMIN_CHECK_ROUTE,
       method: 'GET',
     });
-    cachedResult = check;
-    return check;
+
+    // Cache the result
+    cachedUserIsSelectAdmin = !!userIsSelectAdmin;
+
+    // Return the result
+    return userIsSelectAdmin;
   } catch (err) {
-    cachedResult = false;
+    // Error means the user isn't a select admin
+    cachedUserIsSelectAdmin = false;
     return false;
   }
 };
