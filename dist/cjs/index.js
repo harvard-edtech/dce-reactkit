72ac9d030bc78b7ae6c1a51576f0cb0a469f6af0
Gabe Abrams <gabeabrams@gmail.com>
2026-01-29 13:50:45 -0500

    Rebuilt

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -13205,11 +13205,11 @@ const AddOrEditDBEntry = (props) => {
  * @param [adminsOnly] true if the endpoint is for admins only
  * @returns the endpoint path
  */
-const generateEndpointPath = (collectionName, adminsOnly) => {
+const generateEndpointPath = (collectionName, adminsOnly = true) => {
     // Determine prefix based on whether the endpoint is for admins only
     const userPath = adminsOnly ? 'admin' : 'ttm';
     // Return the endpoint path
-    return `/api/${userPath}/dce-reactkit/dbeditor/${collectionName}`;
+    return `/api/${userPath}/dce-commonkit/dbeditor/${collectionName}`;
 };
 
 /**
